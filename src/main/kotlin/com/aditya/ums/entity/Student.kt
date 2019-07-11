856414bc194895249e8f907fd845ba4fc02d58ed
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-11 21:41:18 +0530

    updated student entity

--- src/main/kotlin/com/aditya/ums/entity/Student.kt
+++ src/main/kotlin/com/aditya/ums/entity/Student.kt
@@ -9,64 +9,61 @@ import javax.persistence.*
 @Entity
 @Table(name = "student")
 class Student (
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "id")
-        var id: Long,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long,
 
-        //@MapsId tells Hibernate to use the id column of address as both primary key and foreign key. Also, notice that the @Id column of the Address entity no longer uses the @GeneratedValue annotation.
-        @OneToOne
-        @MapsId
-        var user: User,
+    //@MapsId tells Hibernate to use the id column of address as both primary key and foreign key. Also, notice that the @Id column of the Address entity no longer uses the @GeneratedValue annotation.
+    @OneToOne
+    @MapsId
+    var user: User,
 
-        @Column(name = "roll_no")
-        var rollNo : Int,
+    @Column(name = "roll_no")
+    var rollNo : Int,
 
-        @Column(name = "DOB")
-        var DOB: LocalDate,
+    @Column(name = "DOB")
+    var DOB: LocalDate,
 
-        //todo refactoring
-        @Column(name = "batch")
-        var batch : String,
+    //todo refactoring
+    @Column(name = "batch")
+    var batch : String,
 
-        @Column(name = "local_address")
-        var localAddress: String,
+    @Column(name = "local_address")
+    var localAddress: String,
 
-        //todo refactoring nationality
-        @Column(name = "nationality")
-        var nationality: String,
+    //todo refactoring nationality
+    @Column(name = "nationality")
+    var nationality: String,
 
-        //todo add ESM : Ex-Serviceman
-        //todo add PHC : Physically handicapped
+    //todo add ESM : Ex-Serviceman
+    //todo add PHC : Physically handicapped
 
-        //student family details
-        @Column(name = "fathers_name")
-        var fathersName: String,
+    //student family details
+    @Column(name = "fathers_name")
+    var fathersName: String,
 
-        @Column(name = "fathers_phone")
-        var fathersPhone: String,
+    @Column(name = "fathers_phone")
+    var fathersPhone: String,
 
-        @Column(name = "fathers_income")
-        var fathersIncome: Int,
+    @Column(name = "fathers_income")
+    var fathersIncome: Int,
 
-        @Column(name = "fathers_occupation")
-        var fathersOccupation: String,
+    @Column(name = "fathers_occupation")
+    var fathersOccupation: String,
 
-        @Column(name = "mothers_name")
-        var mothersName: String,
+    @Column(name = "mothers_name")
+    var mothersName: String,
 
-        @Column(name = "mothers_phone")
-        var mothersPhone: String,
+    @Column(name = "mothers_phone")
+    var mothersPhone: String,
 
-        @Column(name = "mothers_income")
-        var mothersIncome: Int? = 0,
+    @Column(name = "mothers_income")
+    var mothersIncome: Int? = 0,
 
-        @Column(name = "mothers_occupation")
-        var mothersOccupation: String,
+    @Column(name = "mothers_occupation")
+    var mothersOccupation: String,
 
-        @Column(name = "family_income")
-        var familyIncome: Int
-
-
-
+    @Column(name = "family_income")
+    var familyIncome: Int
 )
