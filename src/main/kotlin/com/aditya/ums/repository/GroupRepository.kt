689303f6e04615b659e3d2c0c99bd0ccb6cd0650
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-14 22:59:59 +0530

    Group Repository

---
+++ src/main/kotlin/com/aditya/ums/repository/GroupRepository.kt
@@ -0,0 +1,7 @@
+package com.aditya.ums.repository
+
+import com.aditya.ums.entity.Group
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface GroupRepository: JpaRepository<Group, Int> {
+}
