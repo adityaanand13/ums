92ddded2db8edcc8c8c518cf373ebf2aba7eafa7
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-14 23:01:02 +0530

    Semester Repository

---
+++ src/main/kotlin/com/aditya/ums/repository/SemesterRepository.kt
@@ -0,0 +1,7 @@
+package com.aditya.ums.repository
+
+import com.aditya.ums.entity.Semester
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface SemesterRepository: JpaRepository<Semester, Int> {
+}
