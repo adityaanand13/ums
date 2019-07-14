210ae1d7d5cd9500ec7a80ea97e2653653cc2961
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-14 22:49:55 +0530

    BATCH - ex 2017

---
+++ src/main/kotlin/com/aditya/ums/entity/Batch.kt
@@ -0,0 +1,42 @@
+package com.aditya.ums.entity
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "batch")
+class Batch (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    var id: Int? = null,
+
+    @Column(name = "name")
+    var name: String,
+
+    @Column(name = "description")
+    var description: String,
+
+    //batch should be associated with courses
+    @ManyToOne(
+        cascade = [
+            CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.PERSIST,
+            CascadeType.REFRESH
+        ]
+    )
+    @JoinColumn(name = "course_id")
+    var course: Course,
+
+    //one batch can have multiple semesters
+    @OneToMany(
+            mappedBy = "batch",
+            cascade = [
+                CascadeType.DETACH,
+                CascadeType.MERGE,
+                CascadeType.PERSIST,
+                CascadeType.REFRESH
+            ]
+    )//        fetch = FetchType.EAGER,
+    var semesters: MutableList<Semester> = arrayListOf()
+)
