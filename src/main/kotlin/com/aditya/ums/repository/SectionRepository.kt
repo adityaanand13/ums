2a026d49eda9a0a968000b563d256b437385f002
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-14 23:00:24 +0530

    Section Repository

---
+++ src/main/kotlin/com/aditya/ums/repository/SectionRepository.kt
@@ -0,0 +1,7 @@
+package com.aditya.ums.repository
+
+import com.aditya.ums.entity.Section
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface SectionRepository: JpaRepository<Section, Int> {
+}
