521e873c75d395942ea578c6a4fde7ff2dfc41dc
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-15 07:05:55 +0530

    To get courses by college ID

--- src/main/kotlin/com/aditya/ums/repository/CourseRepository.kt
+++ src/main/kotlin/com/aditya/ums/repository/CourseRepository.kt
@@ -4,4 +4,5 @@ import com.aditya.ums.entity.Course
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CourseRepository : JpaRepository<Course, Int> {
+    fun getAllByCollege_Id(collegeId: Int): List<Course>
 }
