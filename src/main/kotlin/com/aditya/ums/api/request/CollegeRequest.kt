fffb0a09d09ae18bf90b92253824320240464aba
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-15 07:02:34 +0530

    College Request POJO

--- src/main/kotlin/com/aditya/ums/api/request/CollegeRequest.kt
+++ src/main/kotlin/com/aditya/ums/api/request/CollegeRequest.kt
@@ -3,5 +3,6 @@ package com.aditya.ums.api.request
 class CollegeRequest (
         var id: Int,
         var name: String,
-        var descrition: String
+        var descrition: String,
+        var address: String = ""
 )
