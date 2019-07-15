a6bf06dff6a2ce06fcf56b1c0bec7bf63a3c3393
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-15 07:01:23 +0530

    added address field

--- src/main/kotlin/com/aditya/ums/converter/CollegeConverter.kt
+++ src/main/kotlin/com/aditya/ums/converter/CollegeConverter.kt
@@ -1,15 +1,30 @@
 package com.aditya.ums.converter
 
 import com.aditya.ums.api.request.CollegeRequest
+import com.aditya.ums.api.response.CollegeResponse
 import com.aditya.ums.entity.College
 
 class CollegeConverter {
     companion object{
+        fun convertToResponses(colleges: List<College>): List<CollegeResponse>{
+            return colleges.map { college -> convertToResponse(college) }
+        }
+
+        fun convertToResponse(college: College): CollegeResponse{
+            return CollegeResponse(
+                id = college.id,
+                name = college.name,
+                description = college.description,
+                address = college.address,
+                courses = CourseConverter.convertToResponses(college.courses)
+            )
+        }
+
         fun convertToEntity(collegeRequest:CollegeRequest): College{
             return College(
-                id = collegeRequest.id,
                 name = collegeRequest.name,
-                description = collegeRequest.descrition
+                description = collegeRequest.descrition,
+                address = collegeRequest.address
             )
         }
     }
