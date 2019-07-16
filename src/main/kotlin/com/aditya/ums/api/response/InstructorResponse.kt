3182ecdb12315ba8134efccdbcbe4509a86780d5
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-16 07:10:19 +0530

    removed course response

--- src/main/kotlin/com/aditya/ums/api/response/InstructorResponse.kt
+++ src/main/kotlin/com/aditya/ums/api/response/InstructorResponse.kt
@@ -2,6 +2,5 @@ package com.aditya.ums.api.response
 
 class InstructorResponse (
         val employeeID: Int,
-        val user: UserResponse,
-        val coursesResponses: List<CourseResponse>?= null
+        val user: UserResponse
 )
