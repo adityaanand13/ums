1f8c4a2f7eb7497fe27033b0199898a89dd1601a
Aditya Anand <anand.aditya.anand@gmail.com>
2019-07-16 07:15:17 +0530

    created batch request

---
+++ src/main/kotlin/com/aditya/ums/api/request/BatchRequest.kt
@@ -0,0 +1,9 @@
+package com.aditya.ums.api.request
+
+class BatchRequest (
+    var id: Int,
+    var name: String,
+    var description: String
+)
+
+//todo : refactor: add year of study... auto add end year of service based on duration of course
