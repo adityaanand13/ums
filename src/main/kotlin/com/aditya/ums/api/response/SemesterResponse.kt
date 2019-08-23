a9104859ecebf5cc03a43a354281e4cc3a6995f9
Aditya Anand <anand.aditya.anand@gmail.com>
2019-08-23 15:50:58 +0530

    .

--- src/main/kotlin/com/aditya/ums/api/response/SemesterResponse.kt
+++ src/main/kotlin/com/aditya/ums/api/response/SemesterResponse.kt
@@ -1,6 +1,10 @@
 package com.aditya.ums.api.response
 
+import com.aditya.ums.entity.Section
+
 class SemesterResponse (
+    var id: Int? =null,
     var name: String,
-    var description: String
+    var description: String,
+    var sections: List<SectionResponse>
 )
