5e8b2bd4f25b0a5a6577ebad5d01efb766c9a00f
Aditya Anand <anand.aditya.anand@gmail.com>
2019-09-29 22:42:45 +0530

    Authentication Configuration

---
+++ src/main/kotlin/com/aditya/ums/config/SecurityConfig.kt
@@ -0,0 +1,41 @@
+package com.aditya.ums.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.BeanIds
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.core.userdetails.UserDetailsService
+
+
+class SecurityConfig: WebSecurityConfigurerAdapter() {
+    @Autowired val customUserDetailsService: CustomUserDetailService? = null
+
+    @Autowired private val unauthorizedHandler: JwtAuthenticationEntryPoint? = null
+
+    @Bean
+    fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
+        return JwtAuthenticationFilter()
+    }
+
+    @Throws(Exception::class)
+    public override fun configure(authenticationManagerBuilder: AuthenticationManagerBuilder) {
+        authenticationManagerBuilder
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder())
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Throws(Exception::class)
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
