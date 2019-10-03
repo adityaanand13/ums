ebc566e463715c5fdc2c82ae3e7f2a32f9ad571c
Aditya Anand <anand.aditya.anand@gmail.com>
2019-10-03 18:52:14 +0530

    Auth Controller

--- src/main/kotlin/com/aditya/ums/UmsApplication.kt
+++ src/main/kotlin/com/aditya/ums/UmsApplication.kt
@@ -2,18 +2,17 @@ package com.aditya.ums
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
+import java.util.*
+import javax.annotation.PostConstruct
 
-@SpringBootApplication
-class UmsApplication
+@SpringBootApplication(scanBasePackages = ["com.aditya.ums.config", "com.aditya.ums.service", "com.aditya.ums.repository", "com.aditya.ums.security", "com.aditya.ums.api.controller"])
+class UmsApplication{
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    }
 
+}
 fun main(args: Array<String>) {
     runApplication<UmsApplication>(*args)
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder();
-    }
 }
