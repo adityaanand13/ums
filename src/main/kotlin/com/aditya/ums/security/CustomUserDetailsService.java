7d0e51fed02dcbedb9fb09c50071a1b0104af34d
Aditya Anand <adityaanand13@live.com>
2020-03-23 16:47:01 +0530

    Redundant java files

--- src/main/kotlin/com/aditya/ums/security/CustomUserDetailsService.java
+++ src/main/kotlin/com/aditya/ums/security/CustomUserDetailsService.java
@@ -1,37 +1,37 @@
-package com.aditya.ums.security;
-
-import com.aditya.ums.entity.User;
-import com.aditya.ums.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String usernameOrEmail)
-            throws UsernameNotFoundException {
-        // Let people login with either username or email
-        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail);
-        if (user == null) {
-            new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail);
-        }
-
-        return UserPrincipal.create(user);
-    }
-
-    @Transactional
-    public UserDetails loadUserById(Long id) {
-        User user = userRepository.findById(id.intValue()).orElse(null);
-
-        return UserPrincipal.create(user);
-    }
-}
+//package com.aditya.ums.security;
+//
+//import com.aditya.ums.entity.User;
+//import com.aditya.ums.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Override
+//    @Transactional
+//    public UserDetails loadUserByUsername(String usernameOrEmail)
+//            throws UsernameNotFoundException {
+//        // Let people login with either username or email
+//        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail);
+//        if (user == null) {
+//            new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail);
+//        }
+//
+//        return UserPrincipal.create(user);
+//    }
+//
+//    @Transactional
+//    public UserDetails loadUserById(Long id) {
+//        User user = userRepository.findById(id.intValue()).orElse(null);
+//
+//        return UserPrincipal.create(user);
+//    }
+//}
