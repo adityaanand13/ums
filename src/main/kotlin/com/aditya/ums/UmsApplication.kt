d1f409e77302b6ecfae9110853c38ef546d3b71b
Aditya Anand <adityaanand13@live.com>
2020-03-30 02:14:04 +0530

    added file storage config

--- src/main/kotlin/com/aditya/ums/UmsApplication.kt
+++ src/main/kotlin/com/aditya/ums/UmsApplication.kt
@@ -1,16 +1,25 @@
 package com.aditya.ums
 
+import com.aditya.ums.config.FileStorageProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import java.util.*
 import javax.annotation.PostConstruct
 
 @SpringBootApplication(scanBasePackages = ["com.aditya.ums"])
+@EnableConfigurationProperties(FileStorageProperties::class)
 class UmsApplication{
     @PostConstruct
     fun init() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     }
+
+    //todo send to security config
+//    fun configure(http: HttpSecurity){
+//        http.authorizeRequests()
+//                .antMatchers("/actuator/health").permitAll().anyRequest().authenticated()
+//    }
 
 }
 fun main(args: Array<String>) {
