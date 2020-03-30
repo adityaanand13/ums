a96637ce8f92ec13bdb0cf7c6cbe960f5982c5c8
Aditya Anand <adityaanand13@live.com>
2020-03-31 02:12:04 +0530

    updated CourseResponse.kt for Course code

--- src/main/kotlin/com/aditya/ums/api/response/CourseResponse.kt
+++ src/main/kotlin/com/aditya/ums/api/response/CourseResponse.kt
@@ -3,6 +3,7 @@ package com.aditya.ums.api.response
 class CourseResponse(
         var id: Int?= null,
         var name : String,
+        var code: String,
         var description: String,
         var duration: Int,
         var batches: List<BatchResponse>
