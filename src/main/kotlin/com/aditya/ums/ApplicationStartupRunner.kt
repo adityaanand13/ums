a9308315294680ac5409d33190da8b067d25e42a
Aditya Anand <adityaanand13@live.com>
2020-04-17 16:08:42 +0530

    intital commit

--- src/main/kotlin/com/aditya/ums/ApplicationStartupRunner.kt
+++ src/main/kotlin/com/aditya/ums/ApplicationStartupRunner.kt
@@ -28,27 +28,28 @@ class ApplicationStartupRunner : CommandLineRunner {
             logger.info("super admin already exist")
         }else{
             try {
-                // creates a console object
-                val console: Console? = System.console()
-
-                // if console is not null
-                if (console != null) {
-                    do {
-                        println("Enter Super Admin Password")
-                        val pwd1 = console.readPassword("Password: ")
-                        val pwd2 = console.readPassword("Verify Password: ")
-                        val p1 = String(pwd1)
-                        val p2 = String(pwd2)
-                        if (p1 == p2) {
-                            println("Password verified")
-                            println("creating user")
-                            userService.createSuperUser(p1)
-                            break
-                        } else {
-                            println("Password did not match, Try Again")
-                        }
-                    }while (true)
-                }
+                userService.createSuperUser('aditya123')
+//                // creates a console object
+//                val console: Console? = System.console()
+//
+//                // if console is not null
+//                if (console != null) {
+//                    do {
+//                        println("Enter Super Admin Password")
+//                        val pwd1 = console.readPassword("Password: ")
+//                        val pwd2 = console.readPassword("Verify Password: ")
+//                        val p1 = String(pwd1)
+//                        val p2 = String(pwd2)
+//                        if (p1 == p2) {
+//                            println("Password verified")
+//                            println("creating user")
+//                            userService.createSuperUser(p1)
+//                            break
+//                        } else {
+//                            println("Password did not match, Try Again")
+//                        }
+//                    }while (true)
+//                }
             } catch (ex: java.lang.Exception) {
                 // if any error occurs
                 ex.printStackTrace()
